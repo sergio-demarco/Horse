10fbfff029648d8054c4e0bbf38fdc1325e74ef8
Sergio Demarco <demarcosergio1@gmail.com>
2018-03-27 15:48:30 -0300

    Search Clients updated

--- Horse/Horse/UserControls/Clients.Designer.cs
+++ Horse/Horse/UserControls/Clients.Designer.cs
@@ -32,16 +32,25 @@
             this.dataClients = new System.Windows.Forms.DataGridView();
             this.txtClients = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkNombre = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataClients)).BeginInit();
             this.SuspendLayout();
             // 
             // dataClients
             // 
             this.dataClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataClients.Location = new System.Drawing.Point(137, 112);
+            this.dataClients.Location = new System.Drawing.Point(23, 83);
             this.dataClients.Name = "dataClients";
-            this.dataClients.Size = new System.Drawing.Size(798, 388);
+            this.dataClients.Size = new System.Drawing.Size(897, 464);
             this.dataClients.TabIndex = 0;
             // 
             // txtClients
@@ -62,31 +71,120 @@
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // checkedListBox1
+            // checkNombre
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "Nombre",
-            "Apellido",
-            "Correo",
-            "Cuit",
-            "Dni",
-            "Domicilio",
-            "Localidad",
-            "Nombre",
-            "NombreMascota",
-            "RazonSoc",
-            "Telefono"});
-            this.checkedListBox1.Location = new System.Drawing.Point(23, 112);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(108, 169);
-            this.checkedListBox1.TabIndex = 3;
+            this.checkNombre.AutoSize = true;
+            this.checkNombre.Location = new System.Drawing.Point(23, 60);
+            this.checkNombre.Name = "checkNombre";
+            this.checkNombre.Size = new System.Drawing.Size(63, 17);
+            this.checkNombre.TabIndex = 4;
+            this.checkNombre.Text = "Nombre";
+            this.checkNombre.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(92, 60);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Apellido";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(420, 60);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Correo";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(700, 60);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(44, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Cuit";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(557, 60);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(42, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Dni";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(342, 60);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(72, 17);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "Localidad";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(159, 60);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(107, 17);
+            this.checkBox7.TabIndex = 10;
+            this.checkBox7.Text = "Nombre Mascota";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(605, 60);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(89, 17);
+            this.checkBox8.TabIndex = 11;
+            this.checkBox8.Text = "Razon Social";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(483, 60);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(68, 17);
+            this.checkBox9.TabIndex = 12;
+            this.checkBox9.Text = "Telefono";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(265, 60);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(71, 17);
+            this.checkBox10.TabIndex = 13;
+            this.checkBox10.Text = "Direccion";
+            this.checkBox10.UseVisualStyleBackColor = true;
             // 
             // Clients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkNombre);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtClients);
             this.Controls.Add(this.dataClients);
@@ -104,6 +202,15 @@
         private System.Windows.Forms.DataGridView dataClients;
         private System.Windows.Forms.TextBox txtClients;
         private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkNombre;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
     }
 }
